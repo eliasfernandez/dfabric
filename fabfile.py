560c79183516b37bfbee3413179e33106efce15d
Elías <eliasfernandez@gmail.com>
2012-04-02 19:34:51 +0200

    aaaa

---
+++ fabfile.py
@@ -0,0 +1,23 @@
+from __future__ import with_statement
+from fabric.api import *
+from fabric.contrib.console import confirm
+
+#env.hosts = ['my_server']
+
+def prepare_deploy():
+#   local("./manage.py test my_app")
+    local("git add")
+    local("git commit")
+
+def pushpull():
+    local('git push')
+
+def deploy():
+   code_dir = 'httpdocs/dfabric'
+   with settings(warn_only=True):
+       if run("test -d %s" % code_dir).failed:
+          run("git clone git@github.com:eliasfernandez/dfabric.git %s" % code_dir)
+   with cd(code_dir):
+    	run('git pull')
+#	run('touch app.wsgi')
+
